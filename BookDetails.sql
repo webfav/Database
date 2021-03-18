61235b111c2b4278917d4cee05df1815de6d9de4
Jane Jin Larsen <webfav@outlook.dk>
2021-03-18 23:02:26 +0100

    Diagram added

--- BookDetails.sql
+++
@@ -1,7 +0,0 @@
-    SELECT Book.Id AS BookId, Author.Id AS AuthorId, Title, FirstName, LastName, Summary, ISBN
-    FROM [dbo].[Book]
-    LEFT JOIN BookAuthor
-    ON Book.Id = BookAuthor.BookId
-    LEFT JOIN Author
-    ON Author.Id = BookAuthor. AuthorId
-    WHERE book.Id = 2
